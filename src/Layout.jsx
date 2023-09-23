4def31a2c437a4beaab7845b86b7eed1b7db95a1
Matej Bačo <matejbaco2000@gmail.com>
2023-09-23 20:06:01 +0200

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,7 +7,7 @@ export function Layout({ children }) {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Title</title>
+        <title>Title 2</title>
       </head>
       <body>
         <Header />
