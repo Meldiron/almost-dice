d1f42433ed0d6c6d18ae0ab310bc85412f4cc566
Matej Bačo <matejbaco2000@gmail.com>
2023-09-23 20:05:01 +0200

    Remove fragments

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,24 +1,19 @@
-import * as elements from 'typed-html';
+import * as elements from "typed-html";
 import { Header } from "./Header";
 
 export function Layout({ children }) {
   return (
-    <>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>Title</title>
-        </head>
-        <body>
-          <Header />
-          <hr />
-          {children}
-        </body>
-      </html>
-    </>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Title</title>
+      </head>
+      <body>
+        <Header />
+        <hr />
+        {children}
+      </body>
+    </html>
   );
 }
