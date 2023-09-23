e6e7be0d0b5e0ad49f8aa5bc7f3215cdadf61636
Matej Bačo <matejbaco2000@gmail.com>
2023-09-23 19:42:04 +0200

    Add HTML response

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,14 @@
 import * as elements from 'typed-html';
 
+const setup = (context) => {
+  context.res.html = (html, statusCode = 200, headers = {}) => {
+    headers['Content-Type'] = 'text/html';
+    return context.res.send(html, statusCode, headers);
+  };
+}
+
 export default async (context) => {
-  return context.res.send(<h1>Hello World</h1>);
+  setup(context);
+  
+  return context.res.html(<h1>Hello World</h1>);
 };
