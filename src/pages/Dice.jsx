c79427078bf99416f18d049668bf9065ad7f677c
Matej Bačo <matejbaco2000@gmail.com>
2023-09-25 15:51:57 +0200

    Finish dice page design

--- src/pages/Dice.jsx
+++ src/pages/Dice.jsx
@@ -41,17 +41,61 @@ export function Dice() {
             </div>
           </form>
         </div>
-        <div class="col-span-8">
+        <div class="col-span-8 flex flex-col">
           <div class="h-full flex-1 bg-white rounded-lg p-6">
-            <div class="flex justify-center">
-              <div class="border border-stone-200 rounded-md p-6 text-2xl font-bold">
+            <div class="flex justify-center items-center h-full">
+              <div class="border border-stone-200 rounded-md p-6 px-12 text-2xl font-bold">
                 ?
               </div>
             </div>
           </div>
 
           <div class="shrink-0 mt-6 bg-white rounded-lg p-6">
-            <p>Bet options</p>
+            <div class="w-full grid grid-cols-12 gap-6">
+              <div class="col-span-4">
+                <label class="font-semibold">Multiplier:</label>
+                <input
+                  type="number"
+                  value="2"
+                  class="mt-2 w-full p-2 bg-white border border-stone-300 rounded-md"
+                />
+              </div>
+              <div class="col-span-4">
+                <label class="font-semibold">Roll Over:</label>
+                <div class="mt-2 flex items-center justify-center space-x-2">
+                  <button class="p-2 bg-white text-stone-900 border border-stone-200 rounded-md">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+                      />
+                    </svg>
+                  </button>
+                  <input
+                    type="number"
+                    disabled={true}
+                    value="52"
+                    class="w-full p-2 bg-stone-200 border border-stone-300 rounded-md"
+                  />
+                </div>
+              </div>
+              <div class="col-span-4">
+                <label class="font-semibold">Win Chance:</label>
+                <input
+                  type="number"
+                  value="52"
+                  class="mt-2 w-full p-2 bg-white border border-stone-300 rounded-md"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
